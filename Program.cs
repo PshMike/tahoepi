2ffca2d2cff34156ae421e015de10bc9ba238504
pshMike <pshMike@outlook.com>
2024-05-06 00:11:48 -0500

    adding serilog

--- Program.cs
+++ Program.cs
@@ -6,13 +6,10 @@ using Iot.Device.GrovePiDevice.Sensors;
 using System.Threading;
 using System.Device.I2c;
 using Iot.Device.Mcp25xxx.Register.MessageReceive;
-
-
-
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Line 2");
+using Iot.Device.CharacterLcd;
+using System.Drawing;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 
 
@@ -21,13 +18,9 @@ using GrovePi grovePi = new GrovePi(I2cDevice.Create(i2CConnectionSettings));
 
 GrovePort portTempSensor  = GrovePort.AnalogPin0;
 GrovePort portLightSensor = GrovePort.AnalogPin1;
-GrovePort portRelay = GrovePort.DigitalPin4;
-GrovePort portLed = GrovePort.DigitalPin3;
 
 GroveTemperatureSensor s1 = new GroveTemperatureSensor(grovePi,portTempSensor);
-Relay r1 = new Relay(grovePi,portRelay);
 LightSensor s2 = new LightSensor(grovePi,portLightSensor);
-DigitalOutput led = new DigitalOutput(grovePi,portLed);
 
 
 Console.WriteLine($"Manufacturer :{grovePi.GrovePiInfo.Manufacturer}");
@@ -36,13 +29,19 @@ Console.WriteLine($"Firmware version: {grovePi.GrovePiInfo.SoftwareVersion}");
 
 
 
-bool relayClosed = false;
+Log.Logger = new LoggerConfiguration()
+.WriteTo.File("tahoe-logs.log")
+.CreateLogger();
+
+
+
+
+
+
 
 while (true)
 {
-    Console.Clear();
     //sensorReading = grovePi.AnalogRead(tempsensorport);
-    string tempC = s1.Value.ToString();
     int lightLevel = s2.ValueAsPercent;
 
     double R0 = 100000;
@@ -55,31 +54,13 @@ while (true)
     double temp = 1 / (Math.Log( R/R0 )/B+1/298.15) -273.15;
     temp = temp * 1.8 + 32;
 
+    Log.Information("Temp is {0}", temp.ToString());
+    Log.Information("LightLevel is {0}", lightLevel.ToString());
 
     Console.WriteLine(temp.ToString());
-    Console.WriteLine(tempC);
     Console.WriteLine(lightLevel);
+    Console.WriteLine();
 
-    if ( relayClosed ) 
-    {
-        r1.Off();
-    }
-    else 
-    {
-        r1.On();
-    }
-
-    if ( 75 < temp ) 
-    {
-        led.Value = PinValue.High;
-    }
-    else
-    {
-        led.Value = PinValue.Low;
-    }
-
-
-
-    Thread.Sleep(2000);
+    Thread.Sleep(30000);
 }
 
